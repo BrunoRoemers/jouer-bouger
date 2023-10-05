71fbce8238cfecbf42a4fc70a13b1b50cfa19ad2
Bruno <69008445+BrunoRoemers@users.noreply.github.com>
2023-10-05 18:02:34 +0200

    WIP: Decap CMS manual install

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,39 +1,7 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
-import decapCMS from "astro-netlify-cms";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [
-    tailwind(),
-    decapCMS({
-      // docs: https://github.com/delucis/astro-netlify-cms
-      config: {
-        backend: {
-          name: "github",
-          auth_scope: "public_repo",
-          branch: process.env.PUBLIC_BRANCH ?? "main",
-          repo: "brunoRoemers/jouer-bouger",
-        },
-        local_backend: true,
-        collections: [
-          {
-            name: "cards",
-            label: "Cards",
-            folder: "src/content/cards",
-            format: "yml",
-            create: true,
-            slug: "{{slug}}",
-            identifier_field: "slug",
-            summary: "{{slug}} - {{title}}",
-            fields: [
-              { label: "Title", name: "title", widget: "string" },
-              { label: "Slug", name: "slug", widget: "string" },
-              { label: "Video URL", name: "videoUrl", widget: "string" },
-            ],
-          },
-        ],
-      },
-    }),
-  ],
+  integrations: [tailwind()],
 });
