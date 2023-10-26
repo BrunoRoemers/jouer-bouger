7ff2b40bb964f7a14142f21ee9256984608c08e4
Bruno <69008445+BrunoRoemers@users.noreply.github.com>
2023-10-26 17:55:55 +0200

    Configure fields on backend

--- src/content/config.ts
+++ src/content/config.ts
@@ -3,9 +3,18 @@ import { z, defineCollection } from "astro:content";
 const cards = defineCollection({
   type: "data",
   schema: z.object({
-    title: z.string(),
     slug: z.string(),
+    title: z.string(),
     videoUrl: z.string(),
+    category: z.string().optional(),
+    age: z.string().optional(),
+    placeAndSurface: z.string().optional(),
+    materials: z.string().optional(),
+    duration: z.string().optional(),
+    goal: z.string().optional(),
+    victory: z.string().optional(),
+    variationTwoPlayers: z.string().optional(),
+    variationManyPlayers: z.string().optional(),
   }),
 });
 
